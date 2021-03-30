636bc35d73042c812fdde10b00624d700290d36c
BenjaminSpoiden <spoiden.benjamin@hotmail.com>
2021-03-30 13:32:30 +0200

    implemented searchbar for home fragment

---
+++ app/src/main/java/com/ben/recipestore/ui/BaseFragment.kt
@@ -0,0 +1,26 @@
+package com.ben.recipestore.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB: ViewBinding>: Fragment() {
+
+    private var _binding: VB? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = bindFragment(inflater, container, savedInstanceState)
+        return binding.root
+    }
+
+    abstract fun bindFragment(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): VB
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
--- app/src/main/java/com/ben/recipestore/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ben/recipestore/ui/home/HomeFragment.kt
@@ -2,13 +2,14 @@ package com.ben.recipestore.ui.home
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import com.ben.recipestore.R
+import com.ben.recipestore.databinding.HomeFragmentBinding
+import com.ben.recipestore.ui.BaseFragment
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<HomeFragmentBinding>(), SearchView.OnQueryTextListener {
 
     companion object {
         fun newInstance() = HomeFragment()
@@ -16,11 +17,13 @@ class HomeFragment : Fragment() {
 
     private lateinit var viewModel: HomeViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+    override fun bindFragment(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): HomeFragmentBinding {
+        return HomeFragmentBinding.inflate(inflater, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,4 +32,31 @@ class HomeFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.home_fragment_menu, menu)
+
+        val searchItem = menu.findItem(R.id.home_fragment_search)
+        val searchView = searchItem.actionView as? SearchView
+
+//        searchView?.isSubmitButtonEnabled = true
+        searchView?.maxWidth = Int.MAX_VALUE
+        searchView?.setOnQueryTextListener(this)
+    }
+
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        newText?.let {
+            Log.d("Tag", "$newText")
+        }
+
+        return true
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        query?.let {
+            Log.d("Tag", "Query: $it")
+        }
+        return true
+    }
 }
